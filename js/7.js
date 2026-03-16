035e862254b06e643473552aba7dc32455485491
Abel Almaraz <almarazabeljesus@gmail.com>
2026-03-16 19:05:34 -0300

    ejercicio 7 resuelto

---
+++ js/7.js
@@ -0,0 +1,37 @@
+/*
+7- Haz un script que escriba una pirámide inversa de 
+los números del 1 al número que indique el usuario (no mayor de 50) 
+ de la siguiente forma : (suponiendo que indica 30).
+
+303030303030303030303030303030303030303030303030303030303030
+2929292929292929292929292929292929292929292929292929292929
+28282828282828282828282828282828282828282828282828282828
+…..
+333
+22
+1
+
+*/
+let numero = prompt("Ingrese un número (máximo 50)");
+
+if (isNaN(numero) || numero.trim() === "") {
+    alert("Debe ingresar un número válido");
+} else {
+
+    numero = parseInt(numero);
+
+    if (numero > 50) {
+        alert("El número no puede ser mayor a 50");
+    } else {
+
+        for (let i = numero; i >= 1; i--) {
+
+            for (let j = 1; j <= i; j++) {
+                document.write(i);
+            }
+
+            document.write("<br>");
+        }
+
+    }
+}
