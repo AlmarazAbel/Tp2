04ae3016ef1acd636bab154caca4cf53add81ef5
Abel Almaraz <almarazabeljesus@gmail.com>
2026-03-16 20:03:04 -0300

    ejercicio 17  resuelto

---
+++ js/16.js
@@ -0,0 +1,13 @@
+/*
+Realiza un script que pida una cadena de texto 
+y la devuelva al revés. Es decir, si tecleo 
+“hola que tal” deberá mostrar “lat euq aloh”.
+*/
+let texto = prompt("Ingrese un texto:");
+let invertido = "";
+
+for (let i = texto.length - 1; i >= 0; i--) { //texto.length - 1,empieza a recorrer desde el ultimo caracter
+    invertido += texto[i];
+}
+
+alert("Texto invertido: " + invertido);
